65620328c4a7982c5ec280816ea54f3837aa98ed
mcastrox <00002018@uca.edu.sv>
2019-11-23 17:02:15 -0600

    4prueba

--- src/app.js
+++ src/app.js
@@ -12,6 +12,14 @@ mongoose.connect('mongodb://localhost/crud-mongo')
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));
 
+  // conccion remota 
+
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  }) 
+    .then(db => console.log(`DB is connected`))
+    .catch(err => console.error(err));
+  
 // importing routes
 const indexRoutes = require('./routes/index');
 
